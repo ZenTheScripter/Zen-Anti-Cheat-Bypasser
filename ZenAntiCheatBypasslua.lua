642a526ca27262c9e46655c8123a5f64877d064e
DarkFadow <Belpitoa@gmail.com>
2025-10-31 21:39:17 +0100

    Added First File (TESTING)

---
+++ ZenAntiCheatBypasslua.lua
@@ -0,0 +1,99 @@
+local AntiCheatBypassUltra = {}
+
+-- Fonction pour générer une clé aléatoire
+local function generateRandomKey()
+    math.randomseed(os.time())
+    local key = ""
+    for i = 1, 32 do
+        key = key .. string.char(math.random(32, 126))
+    end
+    return key
+end
+
+-- Fonction pour obfusquer le code de manière polymorphe
+local function polymorphicObfuscateCode(code, key)
+    -- Remplacer les noms de variables et de fonctions par des chaînes aléatoires
+    local obfuscated = code:gsub("%w+", function(word)
+        if word:match("%a") then
+            return "#" .. word:reverse() .. "##" .. key
+        end
+        return word
+    end)
+
+    -- Ajouter des instructions inutiles pour confondre les analyseurs
+    obfuscated = obfuscated .. "\nlocal _ = function() end; _();"
+
+    return obfuscated
+end
+
+-- Fonction pour chiffrer le code de manière asymétrique
+local function asymmetricEncryptCode(code, publicKey)
+    local rsa = require("rsa")
+    local encrypted = rsa.encrypt(code, publicKey)
+    return encrypted
+end
+
+-- Fonction pour déchiffrer le code
+local function decryptCode(encrypted, privateKey)
+    local rsa = require("rsa")
+    local decrypted = rsa.decrypt(encrypted, privateKey)
+    return decrypted
+end
+
+-- Fonction pour injecter du code de manière dynamique en mémoire
+local function injectCodeInMemory(code, key)
+    local encryptedCode = asymmetricEncryptCode(code, key)
+    local obfuscatedCode = polymorphicObfuscateCode(encryptedCode, key)
+    local functionWrapper = loadstring(obfuscatedCode)
+    functionWrapper()
+end
+
+-- Fonction pour ajouter des instructions inutiles
+local function addUselessInstructions(code)
+    local uselessInstructions = {
+        "local _ = function() end; _();",
+        "local a, b = 0, 0; a = b + 1;",
+        "local c = {}; c[1] = 1; c[2] = 2;",
+        "local d = function() end; d();",
+        "local e = {}; e[1] = function() end; e[1]();"
+    }
+    local randomInstruction = uselessInstructions[math.random(#uselessInstructions)]
+    return code .. "\n" .. randomInstruction
+end
+
+-- Fonction pour mélanger les instructions
+local function shuffleInstructions(code)
+    local lines = {}
+    for line in code:gmatch("([^\n]*)\n?") do
+        table.insert(lines, line)
+    end
+    math.randomseed(os.time())
+    for i = #lines, 2, -1 do
+        local j = math.random(i)
+        lines[i], lines[j] = lines[j], lines[i]
+    end
+    return table.concat(lines, "\n")
+end
+
+-- Fonction pour afficher une alerte dans les logs de Roblox
+local function logAlert(message)
+    warn(message)
+end
+
+-- Fonction principale pour activer le bypass
+function AntiCheatBypassUltra:Enable(codeToProtect, publicKey, privateKey)
+    -- Afficher une alerte dans les logs de Roblox
+    logAlert("AntiCheatBypassUltra a été chargé avec succès!")
+
+    -- Mélanger les instructions
+    local shuffledCode = shuffleInstructions(codeToProtect)
+
+    -- Ajouter des instructions inutiles
+    local codeWithUselessInstructions = addUselessInstructions(shuffledCode)
+
+    -- Injecter le code
+    injectCodeInMemory(codeWithUselessInstructions, publicKey, privateKey)
+end
+
+-- Exporter la bibliothèque
+return AntiCheatBypassUltra
